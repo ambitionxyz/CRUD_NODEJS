c0d4aa5a8429393d7eaa15cc8daa38fac3d871f4
quang luan <hoangquangluan23102001@gmail.com>
2023-03-03 19:52:54 +0700

    verify token email

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,10 @@
 const bcrypt = require("bcryptjs");
+const crypto = require("crypto");
 const nodemailer = require("nodemailer");
 const sendgridTransport = require("nodemailer-sendgrid-transport");
 
 const User = require("../models/user");
+const Token = require("../models/token");
 
 const transporter = nodemailer.createTransport(
   //cau hinh ndemailer su dng sendrid
@@ -48,8 +50,18 @@ exports.postLogin = (req, res, next) => {
   User.findOne({ email: email })
     .then((user) => {
       if (!user) {
-        req.flash("error", "Invalid email or password.");
+        req.flash(
+          "error",
+          `The email address ${email} + ' is not associated with any account. please check and try again!`
+        );
         return res.redirect("/login");
+      }
+      // check user is verified or not
+      if (!user.isVerified) {
+        return req.flash(
+          "error",
+          "Your Email has not been verified. Please click on resend"
+        );
       }
       bcrypt
         .compare(password, user.password)
@@ -86,32 +98,105 @@ exports.postSignup = (req, res, next) => {
         );
         return res.redirect("/signup");
       }
-      return bcrypt
-        .hash(password, 12)
-        .then((hashedPassword) => {
-          const user = new User({
-            email: email,
-            password: hashedPassword,
-            cart: { items: [] },
-          });
-          return user.save();
-        })
-        .then((result) => {
-          res.redirect("/login");
-          return transporter.sendMail({
-            to: email,
-            from: "testktrdh@gmail.com",
-            subject: "Signup succeeded!",
-            html: "<h1>You successfully signed up!</h1>",
-          });
-        })
-        .catch((err) => {
-          console.log(err);
+      return bcrypt.hash(password, 12).then((hashedPassword) => {
+        const user = new User({
+          email: email,
+          password: hashedPassword,
+          cart: { items: [] },
         });
+        return user.save(function (err) {
+          // generate token and save
+          var token = new Token({
+            _userId: user._id,
+            token: crypto.randomBytes(16).toString("hex"),
+          });
+          // Send email (use credintials of SendGrid)
+          var mailOptions = {
+            from: "testktrdh@gmail.com",
+            to: email,
+            subject: "Account Verification Link",
+            text:
+              "Hello user" +
+              ",\n\n" +
+              "Please verify your account by clicking the link: \nhttp://" +
+              req.headers.host +
+              "/confirmation/" +
+              user.email +
+              "/" +
+              token.token +
+              "\n\nThank You!\n",
+          };
+          transporter.sendMail(mailOptions, function (err) {
+            console.log("mail options", mailOptions);
+            if (err) {
+              return res.status(500).send({
+                msg: "Technical Issue!, Please click on resend for verify your Email.",
+              });
+            }
+            return res
+              .status(200)
+              .send(
+                "A verification email has been sent to " +
+                  user.email +
+                  ". It will be expire after one day. If you not get verification Email click on resend token."
+              );
+          });
+        });
+      });
     })
     .catch((err) => {
       console.log(err);
     });
+};
+
+// It is GET method, you have to write like that
+//    app.get('/confirmation/:email/:token',confirmEmail)
+
+exports.confirmEmail = function (req, res, next) {
+  Token.findOne({ token: req.params.token }, function (err, token) {
+    // token is not found into database i.e. token may have expired
+    if (!token) {
+      return res.status(400).send({
+        msg: "Your verification link may have expired. Please click on resend for verify your Email.",
+      });
+    }
+    // if token is found then check valid user
+    else {
+      User.findOne(
+        { _id: token._userId, email: req.params.email },
+        function (err, user) {
+          // not valid user
+          if (!user) {
+            return req.flash(
+              "error",
+              "We were unable to find a user for this verification. Please SignUp!"
+            );
+          }
+          // user is already verified
+          else if (user.isVerified) {
+            // return res
+            //   .status(200)
+            //   .send("User has been already verified. Please Login");
+          }
+          // verify user
+          else {
+            // change isVerified to true
+            user.isVerified = true;
+            user.save(function (err) {
+              // error occur
+              if (err) {
+                return req.flash("error", "ERR!");
+              }
+              // account successfully verified
+              else {
+                return req.redirect("/login");
+              }
+            });
+          }
+        }
+      );
+    }
+  });
 };
 
 exports.postLogout = (req, res, next) => {
